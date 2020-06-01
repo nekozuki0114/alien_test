b5731c68de2ce525dddb094af4a97aa3b8fdc315
nekozuki0114 <nekozuki.090@gmail.com>
2020-06-01 14:48:38 +0900

    test

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // expressモジュールを読み込む
 const express = require('express');
 
+
 // expressアプリを生成する
 const app = express();
+
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static('public'));
 // ルート（http://localhost/）にアクセスしてきたときに「Hello」を返す
 app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
 
-// ポート3000でサーバを立てる
-app.listen(3000, () => console.log('Listening on port 3000'));
+//サーバを立てる
+app.listen(app.get('port'), () => console.log('Listening on port'+app.get('port')));
